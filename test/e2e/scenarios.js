d8d88c07792036fadd7458d2989db756e8ce8dfb
delcoker <delcoker3@hotmail.com>
2014-09-09 21:20:50 +0000

    Step 4 commit

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -9,23 +9,30 @@ describe('PhoneCat App', function() {
       var phoneList = element.all(by.repeater('phone in phones'));
       var query = element(by.model('query'));
 
-      it('should filter the phone list as user types into the search box', function() {
-        expect(phoneList.count()).toBe(3);
+       it('should be possible to control phone order via the drop down select box', function() {
 
-        query.sendKeys('nexus');
-        expect(phoneList.count()).toBe(1);
+      var phoneNameColumn = element.all(by.repeater('phone in phones').column('{{phone.name}}'));
+      var query = element(by.model('query'));
 
-        query.clear();
-        query.sendKeys('motorola');
-        expect(phoneList.count()).toBe(2);
-      });
+      function getNames() {
+        return phoneNameColumn.map(function(elm) {
+          return elm.getText();
+        });
+      }
 
-      it('should display the current filter value in the title bar', function() {
-        query.clear();
-        expect(browser.getTitle()).toMatch(/Google Phone Gallery:\s*$/);
+      query.sendKeys('tablet'); //let's narrow the dataset to make the test assertions shorter
 
-        query.sendKeys('nexus');
-        expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
-      });
+      expect(getNames()).toEqual([
+        "Motorola XOOM\u2122 with Wi-Fi",
+        "MOTOROLA XOOM\u2122"
+      ]);
+
+      element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
+
+      expect(getNames()).toEqual([
+        "MOTOROLA XOOM\u2122",
+        "Motorola XOOM\u2122 with Wi-Fi"
+      ]);
+    });
     });
   });
